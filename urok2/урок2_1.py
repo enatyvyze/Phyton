bdeb78e87c624efd364040f2885e494982ec1aad
enatyvyze <savka98rus666@gmail.com>
2020-12-24 16:14:41 +0300

    LESS2

---
+++ urok2/урок2_1.py
@@ -0,0 +1,8 @@
+#    0   1      2      3           4                         5
+a = (2, 4.6, 'Hello', True, [9, 9.99, 'Kitty'], {'name':'James', 'age':'29'})
+print(type(a[0]))
+print(type(a[1]))
+print(type(a[2]))
+print(type(a[3]))
+print(type(a[4]))
+print(type(a[5]))
